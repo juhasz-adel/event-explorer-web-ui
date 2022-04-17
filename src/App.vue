885b452e3814d45b821d9ea28f271e5de033c28f
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-17 20:17:55 +0200

    Navbar kinyitásának beprogramozása

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" v-bind:href="'/'">Event Explorer</a>
     <button
-      class="navbar-toggler"
+      v-on:click="toggle()"
+      :class="getNavbarTogglerClass()"
       type="button"
       data-toggle="collapse"
       data-target="#navbarNav"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div :class="getNavbarCollapseClass()">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" v-bind:href="'/'">Események</a>
@@ -23,6 +24,32 @@
 
   <router-view />
 </template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      isOpen: true,
+    };
+  },
+  methods: {
+    getNavbarTogglerClass() {
+      return this.isOpen ? "navbar-toggler" : "navbar-toggler collapsed";
+    },
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+    getNavbarCollapseClass() {
+      return this.isOpen
+        ? "collapse navbar-collapse collapse"
+        : "collapse navbar-collapse collapse show";
+    },
+  },
+  mounted() {},
+};
+</script>
 
 <style>
 #app {
