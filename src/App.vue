25e82f8e3648ccba113b2f78d9c5529b215bce7a
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-17 17:49:13 +0200

    Event komponensek renderelése

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="navbar">
+    <a v-bind:href="'/'"><i class="fa fa-fw fa-search"></i> Események</a>
+    <a v-bind:href="'/profil'"><i class="fa fa-fw fa-user"></i> Profil</a>
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Events from "../views/Events.vue";
+import Profile from "../views/Profile";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Events",
+    component: Events,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/profil",
+    name: "Profile",
+    component: Profile,
   },
 ];
 
