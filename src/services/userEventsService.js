9c82547e7a31fa21c93561ac89944eeb8f603180
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-18 15:33:19 +0200

    Url cím bugfix

--- src/services/userEventsService.js
+++ src/services/userEventsService.js
@@ -1,8 +1,7 @@
 import restClient from "axios";
 
 const getUserEvents = (userId, isUpcoming) => {
-  const baseUrl =
-    "https://localhost:5001/api/users/${userId}/events?isUpcoming=";
+  const baseUrl = `https://localhost:5001/api/users/${userId}/events?isUpcoming=`;
 
   const url = isUpcoming ? `${baseUrl}true` : `${baseUrl}false`;
 
